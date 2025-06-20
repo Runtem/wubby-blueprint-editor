57f575d503b8aac21148b3e20b9e7966c05bf53f
Runtem <mnonomofficial28@gmail.com>
2025-06-20 17:43:27 +0700

    gitignore

---
+++ index.js
@@ -0,0 +1,30 @@
+// main.js
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    icon: "wubby-blueprint-editor.png",
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  })
+  win.maximize()
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
